5cf37b9fb1440d1ad64a89cd72280b9cddfe3161
daivikshetty343 <56639240+daivikshetty343@users.noreply.github.com>
2021-09-01 12:05:26 +0530

    Update cctv2.py

--- home-surveillance-and-intruder-detector-master/cctv2.py
+++ home-surveillance-and-intruder-detector-master/cctv2.py
@@ -42,72 +42,37 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 	frame = f.array
 	timestamp = datetime.datetime.now()
 	text = "Unoccupied"
-
-
-        
 	thresh = fgbg.apply(frame)
-
-
 	thresh = cv2.erode(thresh, (11,11), iterations=5)
-
 	thresh = cv2.dilate(thresh, (11,11), iterations=1)
-
-	
-
-
 	cnts= cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)[-2]
-
 	
 	for c in cnts:
 		t
 		if cv2.contourArea(c) < conf["min_area"]:
 			continue
-
-		
 		(x, y, w, h) = cv2.boundingRect(c)
 		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 		text = "Occupied"
-
-	
 	ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
 	cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
 		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 	cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
 		0.35, (0, 0, 255), 1)
-
-	
 	if text == "Occupied":
-		
-		if (timestamp - lastUploaded).seconds >= conf["min_upload_seconds"]:
-			
-			motionCounter += 1
-
-			
-   
-			
+		if (timestamp - lastUploaded).seconds >= conf["min_upload_seconds"]:		
+			motionCounter += 1	
 			if motionCounter >= conf["min_motion_frames"]:
                                 d+=1
                                 cv2.imwrite('/home/pi/googledrive/intruder'+str(d)+'.png',frame)
-
-					
-				
 				lastUploaded = timestamp
 				motionCounter = 0
-
-	
 	else:
 		motionCounter = 0
-
-	
 	if conf["show_video"]:
-		
 		cv2.imshow("Security Feed", frame)
 		key = cv2.waitKey(1) & 0xFF
-
-		
 		if key == ord("q"):
 			break
-
-	
 	rawCapture.truncate(0)
